4e1aea734245a42c7374bc5bb5a90c6387a36ffc
skeanster <ryanskeans@gmail.com>
2021-10-27 12:20:41 -0700

    MVP 1. User can type in cities, submit, temp is displayed, can switch between Fahrenheit/Celsius

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 const imperial = 'imperial';
+const celcius = 'metric';
+let currentUnits = imperial;
+let currentCity = 'san francisco';
+
 const test = document.querySelector('.test');
 
-async function getWeather(city) {
-  const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${imperial}&APPID=96517cb296bd62cd371f73c7fc1b036e`,
-    { mode: 'cors' },
-  );
-  const weatherData = await response.json();
-  test.textContent = weatherData.main.temp;
-}
+// eslint-disable-next-line func-names
+const display = (function () {
+  const displayData = (data) => {
+    test.textContent = Math.round(data.main.temp);
+  };
 
-getWeather('san francisco');
+  return { displayData };
+}());
+
+const getWeather = async (city, units) => {
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&APPID=96517cb296bd62cd371f73c7fc1b036e`,
+      { mode: 'cors' },
+    );
+    const weatherData = await response.json();
+    display.displayData(weatherData);
+  } catch (error) {
+    alert("Sorry, we couldn't find that city");
+  }
+};
+
+const formSubmit = async (event) => {
+  event.preventDefault();
+  let userInput = document.querySelector('#cityInput').value;
+  currentCity = userInput;
+  await getWeather(currentCity, currentUnits);
+  userInput = '';
+};
+
+const changeUnits = () => {
+  if (currentUnits === imperial) {
+    currentUnits = celcius;
+  } else {
+    currentUnits = imperial;
+  }
+  getWeather(currentCity, currentUnits);
+};
+
+document.querySelector('.projectForm').addEventListener('submit', formSubmit);
+document.querySelector('.changeUnits').addEventListener('click', changeUnits);
+
+getWeather('san francisco', currentUnits);
